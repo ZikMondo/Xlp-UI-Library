0f05bc6883c333561b5cd4aa1eeb6990a7aa147c
Your Name <you@example.com>
2022-01-04 15:09:29 +0500

    Zik

--- src/client/init.client.lua
+++ src/client/init.client.lua
@@ -1,6 +1,14 @@
 local UI = require(script.Library).new("xlp? bruh...")
 
-local Farming = UI:Category("Farming")
+local Test1 = UI:Category("Test1")
+local Test2 = UI:Category("Test2")
+local Test3 = UI:Category("Test3")
 
-local Sector = Farming:Sector("Options")
-local Sector2 = Farming:Sector("Farming")
+local Sector1 = Test1:Sector("?t?4?")
+local Sector1_ = Test1:Sector("?1?t?")
+
+local Sector2 = Test2:Sector("?t?2_?")
+local Sector2_ = Test2:Sector("?t?2?")
+
+local Sector3 = Test3:Sector("?t?2?")
+local Sector3_ = Test3:Sector("?t?3?")

c8284c67d3561a8296faaa3df85ef54cae47aa3b
Your Name <you@example.com>
2022-01-04 07:23:14 +0500

    Zik

---
+++ src/client/init.client.lua
@@ -0,0 +1,6 @@
+local UI = require(script.Library).new("xlp? bruh...")
+
+local Farming = UI:Category("Farming")
+
+local Sector = Farming:Sector("Options")
+local Sector2 = Farming:Sector("Farming")
